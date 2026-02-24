050ee0848be04ac120260752b74f90e1f94d72b9
DanielCodes32 <danialusama057@gmail.com>
2026-02-24 19:51:07 +0200

    My Order

--- lib/core/features/orders/my_orders.dart
+++ lib/core/features/orders/my_orders.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/core/features/Profile/widgets/main_button.dart';
+import 'package:food_delivery_app/core/features/orders/widgets/custom_items.dart';
 import 'package:food_delivery_app/core/features/orders/widgets/secondary_button.dart';
 import 'package:food_delivery_app/core/styles/app_colors.dart';
 import 'package:food_delivery_app/core/styles/text_styles.dart';
@@ -59,95 +60,7 @@ class MyOrders extends StatelessWidget {
             Expanded(
               child: TabBarView(
                 children: [
-                  Padding(
-                    padding: const EdgeInsets.all(24),
-                    child: ListView.separated(
-                      separatorBuilder: (context, index) => SizedBox(height: 24),
-                      itemBuilder: (context, index) => Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Food", style: TextStyles.caption1),
-                          Divider(height: 16, color: AppColors.lightGreyColor1),
-                          SizedBox(height: 16),
-
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Container(
-                                height: 60,
-                                width: 60,
-                                decoration: BoxDecoration(
-                                  color: AppColors.lightGreyColor,
-                                  borderRadius: BorderRadius.circular(8),
-                                ),
-                              ),
-                              SizedBox(width: 14),
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Row(
-                                      children: [
-                                        Text(
-                                          "Pizza Hut",
-                                          style: TextStyles.caption1.copyWith(
-                                            color: AppColors.blackColor,
-                                            fontWeight: FontWeight.w700,
-                                          ),
-                                        ),
-                                        Spacer(),
-                                        Text(
-                                          "#162432",
-                                          style: TextStyles.caption1,
-                                        ),
-                                      ],
-                                    ),
-                                    SizedBox(height: 10),
-                                    Row(
-                                      children: [
-                                        Text(
-                                          "\$35.25",
-                                          style: TextStyles.caption1.copyWith(
-                                            color: AppColors.blackColor,
-                                            fontWeight: FontWeight.w700,
-                                          ),
-                                        ),
-                                        SizedBox(
-                                          height: 16,
-                                          child: VerticalDivider(),
-                                        ),
-                                        Text(
-                                          "03 Items",
-                                          style: TextStyles.caption2,
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 24),
-                          Row(
-                            children: [
-                              SecondaryButton(
-                                title: 'Track Order',
-                                onPressed: () {},
-                              ),
-                              Spacer(),
-                              SecondaryButton(
-                                title: "Cancel",
-                                onPressed: () {},
-                                fillcolor: AppColors.backgroundColor,
-                                textcolor: AppColors.primaryColor,
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      itemCount: 3,
-                    ),
-                  ),
+                  CustomItems(),
                   Center(child: Text("History")),
                 ],
               ),
